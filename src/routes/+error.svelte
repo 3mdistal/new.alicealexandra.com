cc306750a1f05f4ffd472b16847fb12b14d80d71
Alice Moore <86723305+3mdistal@users.noreply.github.com>
2025-05-05 13:14:14 -0400

    Logic is logicking.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -11,12 +11,12 @@
 </script>
 
 <h1>{page.status}</h1>
-{#if status === 404}
+{#if status >= 400 && status < 500}
     <p>Yeah, this page isn't a thing.</p>
     <p>If you feel gaslighted, because it <i>should</i> be a thing, please rest assured that I am 99% likely to be the one among the two of us who has royally fucked up.</p>
     <p>Coding is hard.</p>
     <p>Anyway, have a nice {getDayOfWeek()}.</p>
-{:else if status === 500}
+{:else if status >= 500 && status < 600}
     <p>Ooh, this is a fun error. It just means that I don't know how to connect the different pieces of my code together properly.</p>
     <p>But the fun part is it's not your fault.</p>
     <p>Unless you're a hacker.</p>
